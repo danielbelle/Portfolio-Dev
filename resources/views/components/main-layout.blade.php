28c111ba0cd6d9974a8a8e1333384cc1f3379e63
danielbelle <henrique.danielb@gmail.com>
2025-03-06 09:41:31 -0300

    003 - start segment component layout

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--==================== UNICONS ====================-->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+    <!--==================== SWIPER CSS ====================-->
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+    <!--==================== CSS ====================-->
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+
+    <title>{{ $pageTitle }}</title>
+  </head>
+
+  <body>
+
+    <x-header />
+
+    {{ $slot }}
+
+    <x-footer />
+
+    <!--==================== SCROLL TOP ====================-->
+    <a href="#" class="scrollup" id="scroll-up">
+      <i class="uil uil-arrow-up scrollup_icon"></i>
+    </a>
+
+    <!--==================== SWIPER JS ====================-->
+    <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
+
+    <!--==================== MAIN JS ====================-->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+  </body>
+
+</html>
