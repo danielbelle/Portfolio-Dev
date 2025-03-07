644bf9d28aadcee375b0be62c2a7f9107762dfc9
danielbelle <henrique.danielb@gmail.com>
2025-03-07 15:32:06 -0300

    008 - vercel configuration to show laravel project

--- api/index.php
+++ api/index.php
@@ -2,9 +2,7 @@
 
 // load the laravel applications
 
-require __DIR__ . '/../bootstrap/autoload.php';
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
+require __DIR__ . '/../vendor/autoload.php';
 // Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
