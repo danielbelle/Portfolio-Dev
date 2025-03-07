2c12899e020e37457d2da5fc436b94f5b180ec75
danielbelle <henrique.danielb@gmail.com>
2025-03-07 15:39:51 -0300

    Revert "008 - vercel configuration to show laravel project"
    This reverts commit 644bf9d28aadcee375b0be62c2a7f9107762dfc9.

--- api/index.php
+++ api/index.php
@@ -3,7 +3,6 @@
 // load the laravel applications
 
 require __DIR__ . '/../vendor/autoload.php';
-$app = require_once __DIR__ . '/../vendor/app.php';
 // Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
