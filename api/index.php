d9f7b3e62fa545f10079041c68be7ac4d3857d46
danielbelle <henrique.danielb@gmail.com>
2025-03-07 15:52:54 -0300

    011 - fixing deploy vercel

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 // load the laravel applications
 
 require __DIR__ . '/../public/index.php';
-$app = require_once __DIR__ . '/../public/index.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 // Run the application
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 $response = $kernel->handle(
